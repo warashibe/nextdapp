76fa9f9175c507cd976a45fdc50cbe88485b579e
Tomoya Nagasawa <ocrybit@gmail.com>
2020-05-18 05:20:33 +0200

    abstract Editor and Preview components

--- src/pages/examples/blog.js
+++ src/pages/examples/blog.js
@@ -38,8 +38,7 @@ import cheerio from "cheerio"
 import Dracula from "../../lib/css/Dracula"
 import GithubMarkdown from "../../lib/css/GithubMarkdown"
 
-import { createEditor, Node } from "slate"
-import { Slate, Editable, withReact } from "slate-react"
+import { Node } from "slate"
 const serialize = nodes => nodes.map(n => Node.string(n)).join("\n")
 
 const getElementOffset = element => {
@@ -63,129 +62,11 @@ const offsetTop = () => {
   return scrollTop
 }
 
-const Editor = binder(
-  props => {
-    const [value, setValue] = useState([
-      { type: "paragraph", children: [{ text: "" }] }
-    ])
-    const editor = useMemo(() => withReact(createEditor()), [])
-    useEffect(() => {
-      props.set(editor, "blog_editor")
-      setValue(
-        R.isEmpty(props.init_value)
-          ? [{ type: "paragraph", children: [{ text: "" }] }]
-          : props.init_value
-      )
-      props.set(setValue, "blog_setValue")
-    }, [])
-    const onChange = _value => {
-      setValue(_value)
-      props.set(_value, "blog_editor_value")
-    }
-    return (
-      <Slate editor={editor} value={value} onChange={onChange}>
-        <Editable style={props.editor_style || {}} />
-      </Slate>
-    )
-  },
-  [],
-  ["set"]
-)
+import Editor from "../../../components/Editor"
 
 import ReactHtmlParser from "react-html-parser"
 const marked = require("marked")
-const Preview = props => {
-  const value = props.value || []
-  const serialize = nodes => nodes.map(n => Node.string(n)).join("\n")
-  let __html = ReactHtmlParser(marked(serialize(value)))
-  let AceEditor
-  if (props.mounted) {
-    AceEditor = require("react-ace").default
-    require("brace/theme/monokai")
-  }
-  let index = 0
-  __html = R.map(v => {
-    if (
-      v.type === "pre" &&
-      R.xNil(v.props.children[0]) &&
-      v.props.children[0].type === "code"
-    ) {
-      const text = v.props.children[0].props.children[0]
-      const language = R.isNil(v.props.children[0].props.className)
-        ? ""
-        : v.props.children[0].props.className.split("-")[1]
-      if (language !== "") {
-        try {
-          require(`brace/mode/${language}`)
-        } catch (e) {}
-      }
-      const i = index
-      const onChange2 = code => {
-        const codes = code.split("\n")
-        let incode = false
-        let ci = 0
-        let ci2 = 0
-        let start = null
-        let end = null
-        for (let c of value) {
-          const txt = c.children[0].text
-          if (txt.match(/^```/) !== null && txt.match(/^```.*```/) === null) {
-            if (incode === false) {
-              incode = true
-              if (i === ci2) {
-                start = ci
-              }
-            } else {
-              if (i === ci2) {
-                end = ci
-              }
-              ci2 += 1
-              incode = false
-            }
-          }
-          ci += 1
-        }
-        let new_value =
-          R.xNil(start) && R.xNil(end) && end - start > 1
-            ? R.remove(start + 1, end - start - 1, value)
-            : value
-        new_value = R.insertAll(
-          start + 1,
-          R.map(t => {
-            return { type: "paragraph", children: [{ text: t }] }
-          })(codes)
-        )(new_value)
-        props.setValue(new_value)
-        props.set(new_value, "blog_editor_value")
-      }
-      return (
-        <AceEditor
-          style={{ marginBottom: "16px" }}
-          width="100%"
-          mode={language}
-          value={text}
-          theme="monokai"
-          maxLines={Infinity}
-          onChange={onChange2}
-          name={`code_${index++}`}
-          editorProps={{ $blockScrolling: true }}
-        />
-      )
-    } else {
-      return v
-    }
-  })(__html)
-  return (
-    <Box
-      p={3}
-      className="markdown-body"
-      height={props.height}
-      sx={{ overflow: "auto" }}
-    >
-      {__html}
-    </Box>
-  )
-}
+import Preview from "../../../components/Preview"
 
 export default binder(
   props => {
