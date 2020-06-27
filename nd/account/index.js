7ffec81fe9885d92eeb1e92186763f76c357a8c9
Tomoya Nagasawa <ocrybit@gmail.com>
2020-06-27 05:27:43 +0200

    fix account init import

--- nd/account/index.js
+++ nd/account/index.js
@@ -1,4 +1,6 @@
 const account = require("./account")
-import { default as init } from "./init"
-import { mergeAll } from "ramda"
-module.exports = mergeAll([init, account])
+import init from "./init"
+module.exports = {
+  ...account,
+  init
+}
