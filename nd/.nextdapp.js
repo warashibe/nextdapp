ced62201be8bc1694ea6ad7420ac7cd71e784958
Tomoya Nagasawa <ocrybit@gmail.com>
2020-06-29 06:54:14 +0200

    add no database (nodb) option to user management

--- nd/.nextdapp.js
+++ nd/.nextdapp.js
@@ -1,4 +1,5 @@
-export { getURL as getURL } from "nd/util"
 export { login as login, logout as logout, deleteAccount as deleteAccount, watchUser as watchUser } from "nd/account"
-export { initWeb3 as initWeb3, erc20 as erc20, contract as contract } from "nd/web3"
+export { } from "nd/core"
 export { initFB as initFB, db as db } from "nd/fb"
+export { getURL as getURL } from "nd/util"
+export { initWeb3 as initWeb3, erc20 as erc20, contract as contract } from "nd/web3"
