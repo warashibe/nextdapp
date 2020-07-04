f6fcc4e6f65d2e4a4b09b0acb08b23251898e265
Tomoya Nagasawa <ocrybit@gmail.com>
2020-07-04 19:04:21 +0200

    make global functions chainable

--- nd/core/func.js
+++ nd/core/func.js
@@ -1,7 +1,7 @@
-import { map, prop, is } from "ramda"
+import { clone, mergeLeft, map, prop, is } from "ramda"
 import setter from "./setter"
 import { conf, global } from "nd"
-export default (arr = [], fn) => () => {
+const func = (arr = [], fn) => {
   if (is(Function)(arr)) {
     fn = arr
     arr = fn.props || []
@@ -13,15 +13,24 @@ export default (arr = [], fn) => () => {
       arr = arr || fn.props || []
     }
   }
-  const { binder, set } = setter(arr)
-  const values = map(prop("get"))(binder)
-  return _val =>
-    fn({
-      global,
-      conf: conf,
-      val: _val || {},
-      set,
-      props: values,
-      state$: { values: values }
-    })
+  const _fn = () => {
+    const { binder, set } = setter(arr)
+    return _val => {
+      const args = {
+        global,
+        conf: conf,
+        val: _val || {},
+        set,
+        props: map(prop("get"))(binder)
+      }
+      const _fn = fn => {
+        return val => fn(mergeLeft({ val: val, fn: _fn }, clone(args)))
+      }
+      return fn(mergeLeft({ fn: _fn }, args))
+    }
+  }
+  _fn.props = fn.props
+  return _fn
 }
+
+export default func
