b401c3d22f6814597160450aa6c3e6faf0b8c6b9
Tomoya Nagasawa <ocrybit@gmail.com>
2021-11-25 07:09:40 +0900

    remove latest in favor of bare

--- latest/nd/custom.js
+++
@@ -1,4 +0,0 @@
-export const logConf = async ({ val, set, conf, props, global }) => {
-  console.log(props)
-}
-logConf.props = ["count"]
--- latest/nd/web3/init.js
+++
@@ -1,7 +0,0 @@
-export default {
-  web3_address: null,
-  web3_updated: null,
-  web3_network: null,
-  eth_balance: null,
-  web3_init: false
-}
