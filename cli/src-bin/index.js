3551195f2e6f31c5f1b576dc3029fbe89d6ae0be
Tomoya Nagasawa <ocrybit@gmail.com>
2021-11-25 10:26:33 +0900

    remove bare

--- cli/src-bin/index.js
+++ cli/src-bin/index.js
@@ -1,29 +1,20 @@
 #!/usr/bin/env node
 
-const command = process.argv[2]
-const name = process.argv[3]
-const namespace = process.argv[4]
-const remove = require("../lib/remove").default
-const add = require("../lib/add").default
-switch (command) {
-case "keys":
-  const keys = require("../lib/generateKeys").default
-  keys("./", name === "overwrite")
-  break
-  case "create":
-    const create = require("../lib/create").default
-    create(name)
-  break
-  case "refresh":
-    const refresh = require("../lib/refresh").default
-    refresh()
-  break
-  case "add":
-    add(name,namespace)
-    break
-  case "remove":
-    remove(name,namespace)
-  break
-  default:
-    console.log("command not found")
-}
+const [, , cmd, name, namespace] = process.argv
+const { defaultTo, equals, T, cond, map } = require("ramda")
+
+const cmds = [
+  ["keys", ["./", name === "overwrite"], "generateKeys"],
+  ["create", [name]],
+  ["refresh"],
+  ["add", [name, namespace]],
+  ["remove", [name, namespace]],
+]
+
+cond([
+  ...map(v => [
+    equals(v[0]),
+    () => require(`../lib/${v[2] || v[0]}`).default(...(v[1] || [])),
+  ])(cmds),
+  [T, () => console.log(`command not found: ${cmd}`)],
+])(cmd)
